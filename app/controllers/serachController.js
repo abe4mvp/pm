7dabba06e489493121e5cc54dd3c900fecc92d4a
Abe Schonfeld <abe.schonfeld@gmail.com>
2014-03-16 17:14:18 -0400

    additional styling and search results formatting

--- app/controllers/serachController.js
+++ app/controllers/serachController.js
@@ -4,12 +4,12 @@ var Article = mongoose.model('Article');
 exports.init = function (app) {
 	app.get('/search', function (req, res) {
 		var q = req.query.q
-		Article.find({ body: new RegExp(q, 'i') }, function (error, articles) {
+		Article.find({ title: new RegExp(q, 'i') }, function (error, articles) {
 			if (error) {
 				res.send("No results found");
 			} else {
 				if (articles.length > 0){
-					res.render("articles/index.ejs", { articles: articles });
+					res.render("articles/searchResults.ejs", { articles: articles });
 				} else {
 					res.send("Sorry, no results matching your query")
 				}
