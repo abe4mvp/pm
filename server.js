dfe605318c76ac9fbe2af0b9cfbf35df1473e285
Abe Schonfeld <abe.schonfeld@gmail.com>
2014-03-15 20:11:34 -0400

    add cookies for viewing articles

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var app =  express();
 
+app.use(express.cookieParser());
+
 app.get('/', function (req, res) {
 	res.send('hello World!')
 })
 
 app.get('/articles/:id', function (req, res) {
 	var article_id = req.params.id;
+	
+	var reqCookie = req.cookies.policy_mic || {};
+	var readingList = reqCookie.readingList || [];
+	
+	if(readingList.indexOf(article_id) === -1) readingList.push(article_id);
+	var resCookie = {readingList: readingList}
+	
+	res.cookie("policy_mic", resCookie)
+	
+	console.log(readingList)
 	res.send("You are viewing article number: " + article_id)
 })
 
