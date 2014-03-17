5b52865fe35e8940bad3b1c4fdb589c4da09a6a7
Abe Schonfeld <abe.schonfeld@gmail.com>
2014-03-16 20:45:40 -0400

    addiotional styling

--- seeds.js
+++ seeds.js
@@ -5,17 +5,28 @@ Article.remove({}, function (error) {
 	console.log("db being reset")
 })
 
-var first = new Article({
-	title: "Abe to clone PolicyMic",
+var seeds = [
+
+new Article({
+	title: "Abe is Learning node",
+	body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+	img: "http://nodejs.org/images/logos/nodejs-1280x1024.png"
+}),
+
+new Article({
+	title: "Simple PolicyMic clone created using Node/express",
 	body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 	img: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTOMnYfnoDOm2E9VZCYObNrZZdrYTnj21-7d-O8-FIBJ0m6CzHo"
-})
+}),
 
-var second = new Article({
-	title: "PolicyMic to hire Abe",
+new Article({
+	title: "PolicyMic to hire Abe?",
 	body: "....hopefully ",
-	img:"http://m.c.lnkd.licdn.com/mpr/pub/image-QNz8859HiiBzZXqw-2dQ7JLpyII8XhOSzZzIsAiJy7gCBbaGQNzID7NHysyhBP1gGn4v/abraham-schonfeld.jpg"
+	img:"https://1.gravatar.com/avatar/e9532893b2dc8a5a57d77d839a646e3b?d=https%3A%2F%2Fidenticons.github.com%2F6d518885f8938c48cc75aa8a4b6bdb49.png&r=x&s=460"
 })
+]
+
+
 
 var saveCb = function (error, Article) {
 	if (error) {
@@ -25,5 +36,6 @@ var saveCb = function (error, Article) {
 	}
 }
 
-first.save(saveCb);
-second.save(saveCb);
+seeds.forEach(function (article) {
+	article.save(saveCb);
+})
